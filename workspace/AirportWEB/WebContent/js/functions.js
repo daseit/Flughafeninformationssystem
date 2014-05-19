c633eff48b8e6ad3eefac7280c13d5ded9830b5c
Benjamin Rupp <beruit01@hs-esslingen.de>
2014-05-19 10:15:08 +0200

    fix(main): add final workspace

--- workspace/AirportWEB/WebContent/js/functions.js
+++
@@ -1,6 +0,0 @@
-function toggleDiv(element){
- if(document.getElementById(element).style.display == 'none')
-  document.getElementById(element).style.display = 'block';
- else
-       document.getElementById(element).style.display = 'none';
-}
