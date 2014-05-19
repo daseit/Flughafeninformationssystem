915bdf9d71cef9edbd35f45888ffdeb656e94c50
stluit01 <stluit01@hs-esslingen.de>
2014-05-19 12:20:12 +0200

    Added Attributes and Link References

--- src/AirportEJB/ejbModule/de/airport/ejb/model/Airplane.java
+++ src/AirportEJB/ejbModule/de/airport/ejb/model/Airplane.java
@@ -1,13 +1,39 @@
 package de.airport.ejb.model;
 
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
 @javax.persistence.Entity (name="airplane")
-public class Airplane {
+public class Airplane implements Serializable {
     @javax.persistence.Id
-    @javax.persistence.GeneratedValue
+    @javax.persistence.GeneratedValue(strategy=GenerationType.AUTO)//, generator="my_airplane_seq_gen")
+//	@SequenceGenerator(name="my_airplane_seq_gen", sequenceName="MY_AIRPLANE_SEQ")
 	private int id;
     
 	private String name;
 	
+	@ManyToOne
+	@JoinColumn(name="airline_id")
+	private Airline airline;
+	
+	@OneToOne
+	@JoinColumn(name="runway_id")
+	private Runway runway;
+	
+	@OneToOne
+	@JoinColumn(name="parkingPosition_id")
+	private ParkingPosition parkingPosition;
+	
+	public Airline getAirline() {
+		return airline;
+	}
+
+	public void setAirline(Airline airline) {
+		this.airline = airline;
+	}
+
 	public int getId() {
 		return id;
 	}
---
+++ src/AirportEJB/ejbModule/de/airport/ejb/model/AirplaneType.java
@@ -0,0 +1,25 @@
+package de.airport.ejb.model;
+
+import javax.persistence.GenerationType;
+
+public class AirplaneType {
+	
+	@javax.persistence.Id
+    @javax.persistence.GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
